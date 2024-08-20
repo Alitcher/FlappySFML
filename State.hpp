14432056b6096201366919441aed954c634ce15f
Alicia Sudlerd <alicia.sudlerd@ut.ee>
2024-08-20 18:46:26 +0300

    Create the state machine

---
+++ State.hpp
@@ -0,0 +1,37 @@
+#pragma once
+namespace AlitcherLab 
+{
+	class State
+	{
+	public:
+		virtual void Init( ) = 0;
+		virtual void HanpleInput() = 0;
+		virtual void Update() = 0;
+		virtual void Render(float dt) = 0;
+
+		virtual void Pause();
+		virtual void Resume();
+
+		State();
+		~State();
+
+	private:
+
+	};
+
+	inline void State::Pause()
+	{
+	}
+
+	inline void State::Resume()
+	{
+	}
+
+	State::State()
+	{
+	}
+
+	State::~State()
+	{
+	}
+}
