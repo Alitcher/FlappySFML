275ab9ef113122c2cb09b02b5c7b7ed166b24ace
Alicia Sudlerd <alicia.sudlerd@ut.ee>
2024-08-20 18:49:14 +0300

    Init gamemanager

---
+++ Game.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <memory>
+#include <string>
+#include <SFML/Graphics.hpp>
+#include "StateMachine.hpp"
+#include "AssetManager.hpp"
+#include "InputManager.hpp"
+
+namespace AlitcherLab 
+{
+
+}
