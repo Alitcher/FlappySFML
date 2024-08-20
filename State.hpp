3884ef56c35c1c13f08fcd037e3f28cec2398218
Alicia Sudlerd <alicia.sudlerd@ut.ee>
2024-08-21 00:47:31 +0300

    add deltatime to the update method

--- State.hpp
+++ State.hpp
@@ -6,7 +6,7 @@ namespace AlitcherLab
 	public:
 		virtual void Init( ) = 0;
 		virtual void HanpleInput() = 0;
-		virtual void Update() = 0;
+		virtual void Update(float dt) = 0;
 		virtual void Render(float dt) = 0;
 
 		virtual void Pause();
