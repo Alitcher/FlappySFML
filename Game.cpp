8636f7a64a682400803cf31bee7dc4e510549dc6
Alicia Sudlerd <alicia.sudlerd@ut.ee>
2024-08-21 00:48:04 +0300

    Implement GameManager

--- Game.cpp
+++ Game.cpp
@@ -1,1 +1,46 @@
+#include "Game.hpp"
 #pragma once
+
+namespace AlitcherLab
+{
+	AlitcherLab::Game::Game(int width, int height, std::string title)
+	{
+		_data->window.create(sf::VideoMode(width, height), title, sf::Style::Close | sf::Style::Titlebar);
+		this->Run();
+
+	}
+
+	AlitcherLab::Game::~Game()
+	{
+	}
+
+	void AlitcherLab::Game::Run()
+	{
+		float newTime, frameTime, lerp;
+		float currentTime = this->_clock.getElapsedTime().asSeconds();
+		float accumulator = 0.0f;
+
+		while (this->_data->window.isOpen())
+		{
+			this->_data->machine.ProcessStateChanges(); //check if state changes
+			newTime = this->_clock.getElapsedTime().asSeconds();
+			frameTime = newTime - currentTime;
+
+			if (frameTime > 0.25f)
+			{
+				frameTime = 0.25f;
+			}
+
+			while (accumulator >= dt)
+			{
+				this->_data->machine.GetActiveState()->HanpleInput();
+				this->_data->machine.GetActiveState()->Update(dt);
+
+				accumulator -= dt;
+			}
+
+			lerp = accumulator / dt;
+			this->_data->machine.GetActiveState()->Render(lerp);
+		}
+	}
+}
--- Game.hpp
+++ Game.hpp
@@ -8,5 +8,29 @@
 
 namespace AlitcherLab 
 {
+	struct GameData
+	{
+		StateMachine machine;
+		sf::RenderWindow window;
+		AssetManager assets;
+	};
+
+	typedef std::shared_ptr<GameData> GameDataRef;
+
+	class Game
+	{
+	public:
+		Game(int width, int height, std::string title);
+		~Game();
+
+	private:
+		// Updates run at 60 per second.
+		const float dt = 1.0f / 60.0f;
+		sf::Clock _clock;
+
+		GameDataRef _data = std::make_shared<GameData>();
+
+		void Run();
+	};
 
 }
